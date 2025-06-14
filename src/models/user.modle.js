3d32137bb9c77a992fec9b44bdde5cb84f050dbc
Sourya <souryagupta2005@gmail.com>
2025-06-14 23:22:03 +0530

    created use and video model

---
+++ src/models/user.modle.js
@@ -0,0 +1,58 @@
+import mongoose, {Schema} from 'mongoose';
+import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
+
+const userSchema = new Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+        lowercase: true,
+        trime: true,
+        index: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        lowercase: true,
+        trime: true,
+    },
+    fullname: {
+        type: String,
+        required: true,
+        trime: true,
+        index: true,
+    },
+    avatar: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    coverImage: {
+        type: String,
+    },
+    watchHistory: [{
+        type:Schema.Types.ObjectId,
+        ref: 'video'
+    }],
+    password: {
+        type: String,
+        required: [true, "Password is required"],
+    },
+    refereshToken: {
+        type: String
+    }
+}, {timestamps: true})
+
+
+
+userSchema.pre("save", async function(next){
+    if (!this.isModified("password")) return next()
+        
+    this.password = bcrypt.hash(this.password, 10)
+    next()
+})
+
+
+export const User = mongoose.model("User", userSchema)
